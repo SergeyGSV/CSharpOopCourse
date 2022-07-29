eb271d1502d94770ed50336cf8410dd93602dbd2
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-07-29 16:13:36 +0700

    Внёс небольшие правки

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -1,39 +1,41 @@
 ﻿using System;
 
-namespace RangeTask
+namespace Academits.Gudkov
 {
-    internal class Program
+    namespace RangeTask
     {
-        static void Main()
+        internal class Program
         {
-            // ЛЕКЦИЯ 7
-            // ЗАДАЧА стр. 56 «Range»
-
-            Console.Write("Укажите начало диапазона: ");
-            double from = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Укажите конец диапазона: ");
-            double to = Convert.ToDouble(Console.ReadLine());
-
-            Range range = new Range(from, to);
-
-            Console.WriteLine($"Длина указанного диапазона = {range.GetLength()}");
-
-            Console.Write("Укажите число для проверки: ");
-
-            if (range.IsInside(Convert.ToDouble(Console.ReadLine())))
+            static void Main()
             {
-                Console.WriteLine("Число лежит в указанном диапазоне");
-            }
-            else
-            {
-                Console.WriteLine("Число лежит за пределами указанного диапазона");
-            }
+                // Курсовая 1. Часть 1
 
-            range.From = 12;
-            range.To = 100;
+                Console.Write("Укажите начало диапазона: ");
+                double from = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"Установлены новые значения диапазона: From = {range.From}, To = {range.To}");
+                Console.Write("Укажите конец диапазона: ");
+                double to = Convert.ToDouble(Console.ReadLine());
+
+                Range range = new Range(from, to);
+
+                Console.WriteLine($"Длина указанного диапазона = {range.GetLength()}");
+
+                Console.Write("Укажите число для проверки: ");
+
+                if (range.IsInside(Convert.ToDouble(Console.ReadLine())))
+                {
+                    Console.WriteLine("Число лежит в указанном диапазоне");
+                }
+                else
+                {
+                    Console.WriteLine("Число лежит за пределами указанного диапазона");
+                }
+
+                range.From = 12;
+                range.To = 100;
+
+                Console.WriteLine($"Установлены новые значения диапазона: From = {range.From}, To = {range.To}");
+            }
         }
     }
 }
