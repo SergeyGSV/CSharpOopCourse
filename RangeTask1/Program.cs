f1f54bc053d9b75db9d79c6334604afedb79adb8
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-07-29 16:42:32 +0700

    1

---
+++ RangeTask1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Academits.Gudkov
+{
+    namespace RangeTask
+    {
+        internal class Program
+        {
+            static void Main()
+            {
+                // Курсовая 1. Часть 1
+                Console.Write("Укажите начало диапазона: ");
+                double from = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Укажите конец диапазона: ");
+                double to = Convert.ToDouble(Console.ReadLine());
+
+                Range range = new Range(from, to);
+
+                Console.WriteLine($"Длина указанного диапазона = {range.GetLength()}");
+
+                Console.Write("Укажите число для проверки: ");
+
+                if (range.IsInside(Convert.ToDouble(Console.ReadLine())))
+                {
+                    Console.WriteLine("Число лежит в указанном диапазоне");
+                }
+                else
+                {
+                    Console.WriteLine("Число лежит за пределами указанного диапазона");
+                }
+
+                range.From = 12;
+                range.To = 100;
+
+                Console.WriteLine($"Установлены новые значения диапазона: From = {range.From}, To = {range.To}");
+            }
+        }
+    }
+}
---
+++ RangeTask1/Range.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Academits.Gudkov
+{
+    namespace RangeTask
+    {
+        internal class Range
+        {
+            public double From { get; set; }
+
+            public double To { get; set; }
+
+            public Range(double from, double to)
+            {
+                From = from;
+                To = to;
+            }
+
+            public double GetLength()
+            {
+                return To - From;
+            }
+
+            public bool IsInside(double number)
+            {
+                return number >= From && number <= To;
+            }
+        }
+    }
+}
