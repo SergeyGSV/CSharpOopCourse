52fb33ee7e07f181ec847a5811f2c6b921325fe9
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-01 17:36:55 +0700

    Черновик

--- RangeTask2/Program.cs
+++ RangeTask2/Program.cs
@@ -4,9 +4,24 @@ namespace RangeTask2
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Курсовая 1. Часть 2
+
+            Range range1 = new Range(21, 24);
+
+            Range range2 = new Range(25, 34);
+
+            Range intersectionRange = new Range();
+
+            intersectionRange = range1.GetIntersection(range2);
+
+            Range[] unionRangeArray = new Range[10];
+
+            unionRangeArray = range1.GetUnion(range2);
+
+             
         }
     }
 }
--- RangeTask2/Range.cs
+++ RangeTask2/Range.cs
@@ -8,5 +8,132 @@ namespace RangeTask2
 {
     internal class Range
     {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public Range()
+        {
+
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public Range GetIntersection(Range range)
+        {
+            double a1 = this.From;
+            double a2 = this.To;
+            double b1 = range.From;
+            double b2 = range.To;
+            Console.WriteLine(a1 + " " + a2 + " " + b1 + " " + b2);
+
+            if (a1 <= b2 && a2 >= b1)
+            {
+                double x1 = (a1 > b1) ? a1 : b1;
+                double x2 = (a2 < b2) ? a2 : b2;
+                Range intersectionRange = new Range(x1, x2);
+
+
+                Console.WriteLine(x1 + " " + x2);
+                return intersectionRange;
+            }
+
+            return null;
+        }
+
+        public Range[] GetUnion(Range range)
+        {
+            double a1 = this.From;
+            double a2 = this.To;
+            double b1 = range.From;
+            double b2 = range.To;
+            Console.WriteLine(a1 + " " + a2 + " " + b1 + " " + b2);
+
+            if (a1 <= b2 && a2 >= b1)
+            {
+                double x1 = (a1 < b1) ? a1 : b1;
+                double x2 = (a2 > b2) ? a2 : b2;
+                Range unionRange = new Range(x1, x2);
+
+                Console.WriteLine(x1 + " " + x2);
+                return unionRange;
+            }
+
+            Range[] unionRangeArray = new Range[] {this, range};
+                       
+            return unionRangeArray;
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            double a1 = this.From;
+            double a2 = this.To;
+            double b1 = range.From;
+            double b2 = range.To;
+            Console.WriteLine(a1 + " " + a2 + " " + b1 + " " + b2);
+
+            if (a1 <= b2 && a2 >= b1)
+            {
+                double x1 = (a1 < b1) ? a1 : b1;
+                double x2 = (a2 > b2) ? a2 : b2;
+                Range unionRange = new Range(x1, x2);
+
+                Console.WriteLine(x1 + " " + x2);
+                return unionRange;
+            }
+
+            Range[] unionRangeArray = new Range[] { this, range };
+
+            return unionRangeArray;
+        }
+
+        private bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        public string IsNull(Range range)
+        {
+            if (range is null)
+            {
+                return "null";
+            }
+            else
+            {
+                return "Новый интервал {From}:{To}";
+                //Console.WriteLine($"Новый интервал {range.From}:{range.To}");
+            }
+        }
+
+        public void PrintObject(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        public void PrintObject(Range range)
+        {
+            this.From = From;
+            this.To = To;
+
+            if (range is null)
+            {
+                Console.WriteLine($"null");
+            }
+            else
+            {
+                Console.WriteLine($"Новый интервал {From}:{To}");
+                //Console.WriteLine($"Новый интервал {range.From}:{range.To}");
+            }
+        }
+
     }
 }
