79a8d0ae6a44ab85e0c8f440b157e53ab3d7e6d3
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-03 10:14:11 +0700

    Подготовлен шаблон

---
+++ ShapesTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Academits.Gudkov
+{
+    namespace ShapesTask
+    {
+        internal class Program
+        {
+            static void Main()
+            {
+                // Курсовая 1. Задача 1. Часть 1
+
+                
+            }
+        }
+    }
+}
