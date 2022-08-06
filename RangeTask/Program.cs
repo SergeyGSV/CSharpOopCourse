1da0fea515134db2d3e122ad1142b6dbe9fffe12
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-07 06:34:43 +0700

    Частично исправленный по замечаниям

---
+++ RangeTask/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Academits.Gudkov.RangeTask
+{
+    internal class Program
+    {
+        private static void Print(Range range)
+        {
+            if (range is null)
+            {
+                Console.WriteLine("null");
+            }
+            else
+            {
+                Console.WriteLine($"Интервал {range.From}:{range.To}");
+            }
+        }
+
+        private static void Print(Range[] range)
+        {
+            if (range[0] is null)
+            {
+                Console.WriteLine("null");
+            }
+            else if (range.Length == 1)
+            {
+                Console.WriteLine($"Интервал {range[0].From}:{range[0].To}");
+            }
+            else
+            {
+                Console.WriteLine($"Интервалы {range[0].From}:{range[0].To} и {range[1].From}:{range[1].To}");
+            }
+        }
+
+        static void Main()
+        {
+            // Курсовая 1. Часть 1
+            Console.Write("Укажите начало диапазона: ");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Укажите конец диапазона: ");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(from, to);
+
+            Console.WriteLine($"Длина указанного диапазона = {range.GetLength()}");
+
+            Console.Write("Укажите число для проверки: ");
+
+            if (range.IsInside(Convert.ToDouble(Console.ReadLine())))
+            {
+                Console.WriteLine("Число лежит в указанном диапазоне");
+            }
+            else
+            {
+                Console.WriteLine("Число лежит за пределами указанного диапазона");
+            }
+
+            range.From = 12;
+            range.To = 100;
+
+            Console.WriteLine($"Установлены новые значения диапазона: From = {range.From}, To = {range.To}");
+
+            // Task 2
+            // Курсовая 1. Часть 2
+            while (true)
+            {
+                Console.WriteLine("Укажите границы диапазонов");
+                Console.WriteLine();
+
+                Console.Write("Начало первого диапазона: ");
+                double rangeFrom = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Конец первого диапазона: ");
+                double rangeTo = Convert.ToDouble(Console.ReadLine());
+                Range range1 = new Range(rangeFrom, rangeTo);
+                Console.WriteLine();
+
+
+                Console.Write("Начало второго диапазона: ");
+                rangeFrom = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Конец второго диапазона: ");
+                rangeTo = Convert.ToDouble(Console.ReadLine());
+                Range range2 = new Range(rangeFrom, rangeTo);
+                Console.WriteLine();
+
+                Console.WriteLine("Результат пересечения");
+                Print(range1.GetIntersection(range2));
+                Console.WriteLine();
+
+                Console.WriteLine("Результат объединения");
+                Print(range1.GetUnion(range2));
+                Console.WriteLine();
+
+                Console.WriteLine("Результат разности");
+                Print(range1.GetDifference(range2));
+                Console.WriteLine("------------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ RangeTask/Range.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace Academits.Gudkov.RangeTask
+{
+    internal class Range
+    {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        // Task 2
+        public Range GetIntersection(Range range)
+        {
+            if ((this.From < range.To && this.To > range.From) || (this.From == range.From && this.To == range.To))
+            {
+                return new Range(Math.Max(this.From, range.From), Math.Min(this.To, range.To));
+            }
+
+            return null;
+        }
+
+        public Range[] GetUnion(Range range)
+        {
+            if (this.From <= range.To && this.To >= range.From)
+            {
+                return new Range[] { new Range(Math.Min(this.From, range.From), Math.Max(this.To, range.To)) };
+            }
+
+            return new Range[] { this, range };
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            double a1 = this.From;
+            double a2 = this.To;
+            double b1 = range.From;
+            double b2 = range.To;
+
+            if (a1 < b2 && a2 > b1)
+            {
+                if (a1 < b1)
+                {
+                    return new Range[] { new Range(a1, b1) };
+                }
+
+                if (b2 < a2)
+                {
+                    return new Range[] { new Range(b2, a2) };
+                }
+            }
+
+            if (a1 == b1 && a2 == b2)
+            {
+                return new Range[] { };
+            }
+
+            return new Range[] { this, range };
+        }
+    }
+}
