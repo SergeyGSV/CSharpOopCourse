16caae0aadee80243577529a0498a78f945d9123
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-07 05:16:59 +0700

    Скорректировал методы

--- ShapesTask1/Program.cs
+++ ShapesTask1/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 
-namespace Academits.Gudkov
+namespace Academits.Gudkov.ShapesTask
 {
-    namespace ShapesTask
+    internal class Program
     {
-        internal class Program
+        static void Main()
         {
-            static void Main()
-            {
-                // Курсовая 1. Задача 1. Часть 1
+            // Курсовая 1. Задача 1. Часть 1
 
-                IShapes triangle = new Triangle(new Point[] { new Point(2, 4), new Point(1, 5), new Point(12, 41) });
+            IShapes triangle = new Triangle(new Point[] { new Point(2, 4), new Point(1, 5), new Point(12, 41) });
+            Point[] point1 = new Point[3];
+            IShapes triangle2 = new Triangle(point1);
 
-                Console.WriteLine(triangle.GetPerimeter());
+            Console.WriteLine(triangle2.GetWidth());
 
-                Console.WriteLine(triangle.GetArea());
-            }
+            // Console.WriteLine(triangle1.GetHeight());
+
+            // Console.WriteLine(triangle1.GetPerimeter());
+
+            // Console.WriteLine(triangle1.GetArea());
         }
     }
 }
--- ShapesTask1/Shapes.cs
+++ ShapesTask1/Shapes.cs
@@ -1,71 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Academits.Gudkov
+namespace Academits.Gudkov.ShapesTask
 {
-    namespace ShapesTask
+    public interface IShapes
     {
-        public interface IShapes
+        double GetWidth();
+        double GetHeight();
+        double GetArea();
+        double GetPerimeter();
+    }
+
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
         {
-            double GetWidth();
-            double GetHeight();
-            double GetArea();
-            double GetPerimeter();
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Triangle : IShapes
+    {
+        public Point[] Points { get; set; }
+
+        public Triangle(Point[] points)
+        {
+            Points = points;
         }
 
-        public class Point
+        private double[] GetSides()
         {
-            public double X { get; set; }
-            public double Y { get; set; }
-            public Point(double x, double y)
-            {
-                X = x;
-                Y = y;
-            }
+            double ab = Math.Sqrt(Math.Pow(Points[1].X - Points[0].X, 2) + Math.Pow(Points[1].Y - Points[0].Y, 2));
+            double ac = Math.Sqrt(Math.Pow(Points[2].X - Points[0].X, 2) + Math.Pow(Points[2].Y - Points[0].Y, 2));
+            double bc = Math.Sqrt(Math.Pow(Points[2].X - Points[1].X, 2) + Math.Pow(Points[2].Y - Points[1].Y, 2));
+
+            return new double[] { ab, ac, bc };
         }
 
-        public class Triangle : IShapes
+        public double GetWidth()
         {
-            public Point[] Points { get; set; }
+            double[] xPointsArray = new double[] { this.Points[0].X, this.Points[1].X, this.Points[2].X };
 
-            public Triangle(Point[] points)
-            {
-                Points = points;
-            }
-            public double GetWidth()
-            {
-                return 1;
-            }
-            public double GetHeight()
-            {
-                return 2;
-            }
-            public double GetArea()
-            {
-                // double halfPerimeter = (ab + ac + bc) / 2;
-                // double triangleArea = Math.Sqrt(halfPerimeter * (halfPerimeter - ab) * (halfPerimeter - ac) * (halfPerimeter - bc));
+            return xPointsArray.Max() - xPointsArray.Min();
+        }
 
-                double halfPerimeter = (this.GetSides()[0] + this.GetSides()[1] + this.GetSides()[2]) / 2;
+        public double GetHeight()
+        {
+            double[] yPointsArray = new double[] { this.Points[0].Y, this.Points[1].Y, this.Points[2].Y };
 
-                double triangleArea = Math.Sqrt(halfPerimeter * (halfPerimeter - this.GetSides()[0]) * (halfPerimeter - this.GetSides()[1]) * (halfPerimeter - this.GetSides()[2]));
+            return yPointsArray.Max() - yPointsArray.Min();
+        }
 
-                return triangleArea;
-            }
-            private double[] GetSides()
-            {
-                double ab = Math.Sqrt(Math.Pow(Points[1].X - Points[0].X, 2) + Math.Pow(Points[1].Y - Points[0].Y, 2));
-                double ac = Math.Sqrt(Math.Pow(Points[2].X - Points[0].X, 2) + Math.Pow(Points[2].Y - Points[0].Y, 2));
-                double bc = Math.Sqrt(Math.Pow(Points[2].X - Points[1].X, 2) + Math.Pow(Points[2].Y - Points[1].Y, 2));
+        public double GetArea()
+        {
+            double[] sidesArray = this.GetSides();
+            double halfPerimeter = (sidesArray[0] + sidesArray[1] + sidesArray[2]) / 2;
+            double triangleArea = Math.Sqrt(halfPerimeter * (halfPerimeter - sidesArray[0]) * (halfPerimeter - sidesArray[1]) * (halfPerimeter - sidesArray[2]));
 
-                return new double[] { ab, ac, bc };
-            }
-            public double GetPerimeter()
-            {
-                return this.GetSides()[0] + this.GetSides()[1] + this.GetSides()[2];
-            }
+            return triangleArea;
+        }
+
+        public double GetPerimeter()
+        {
+            double[] sidesArray = this.GetSides();
+
+            return sidesArray[0] + sidesArray[1] + sidesArray[2];
         }
     }
 }
