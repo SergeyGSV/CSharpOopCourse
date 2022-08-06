0b041f747a7d3bbcb440d3f562d1ea07d8592a30
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-06 11:45:46 +0700

    Наброски для треугольника

--- ShapesTask1/Program.cs
+++ ShapesTask1/Program.cs
@@ -10,7 +10,11 @@ namespace Academits.Gudkov
             {
                 // Курсовая 1. Задача 1. Часть 1
 
-                
+                IShapes triangle = new Triangle(new Point[] { new Point(2, 4), new Point(1, 5), new Point(12, 41) });
+
+                Console.WriteLine(triangle.GetPerimeter());
+
+                Console.WriteLine(triangle.GetArea());
             }
         }
     }
--- ShapesTask1/Shapes.cs
+++ ShapesTask1/Shapes.cs
@@ -8,8 +8,64 @@ namespace Academits.Gudkov
 {
     namespace ShapesTask
     {
-        internal class Shapes
+        public interface IShapes
         {
+            double GetWidth();
+            double GetHeight();
+            double GetArea();
+            double GetPerimeter();
+        }
+
+        public class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        public class Triangle : IShapes
+        {
+            public Point[] Points { get; set; }
+
+            public Triangle(Point[] points)
+            {
+                Points = points;
+            }
+            public double GetWidth()
+            {
+                return 1;
+            }
+            public double GetHeight()
+            {
+                return 2;
+            }
+            public double GetArea()
+            {
+                // double halfPerimeter = (ab + ac + bc) / 2;
+                // double triangleArea = Math.Sqrt(halfPerimeter * (halfPerimeter - ab) * (halfPerimeter - ac) * (halfPerimeter - bc));
+
+                double halfPerimeter = (this.GetSides()[0] + this.GetSides()[1] + this.GetSides()[2]) / 2;
+
+                double triangleArea = Math.Sqrt(halfPerimeter * (halfPerimeter - this.GetSides()[0]) * (halfPerimeter - this.GetSides()[1]) * (halfPerimeter - this.GetSides()[2]));
+
+                return triangleArea;
+            }
+            private double[] GetSides()
+            {
+                double ab = Math.Sqrt(Math.Pow(Points[1].X - Points[0].X, 2) + Math.Pow(Points[1].Y - Points[0].Y, 2));
+                double ac = Math.Sqrt(Math.Pow(Points[2].X - Points[0].X, 2) + Math.Pow(Points[2].Y - Points[0].Y, 2));
+                double bc = Math.Sqrt(Math.Pow(Points[2].X - Points[1].X, 2) + Math.Pow(Points[2].Y - Points[1].Y, 2));
+
+                return new double[] { ab, ac, bc };
+            }
+            public double GetPerimeter()
+            {
+                return this.GetSides()[0] + this.GetSides()[1] + this.GetSides()[2];
+            }
         }
     }
 }
