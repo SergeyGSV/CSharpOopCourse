cf1e762ee85b37682d876f89e72660d7d2483794
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-07 14:52:21 +0700

    RangeTask. Частично исправлены замечания

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Academits.Gudkov.RangeTask
 {
@@ -8,34 +9,40 @@ namespace Academits.Gudkov.RangeTask
         {
             if (range is null)
             {
-                Console.WriteLine("null");
+                Console.WriteLine($"[]");
             }
             else
             {
-                Console.WriteLine($"Интервал {range.From}:{range.To}");
+                Console.Write($"[{range}]");
             }
         }
 
-        private static void Print(Range[] range)
+        private static void Print(Range[] ranges)
         {
-            if (range[0] is null)
+            if (ranges.Length == 0 || ranges[0] is null)
             {
-                Console.WriteLine("null");
-            }
-            else if (range.Length == 1)
-            {
-                Console.WriteLine($"Интервал {range[0].From}:{range[0].To}");
+                Console.WriteLine($"[]");
             }
             else
             {
-                Console.WriteLine($"Интервалы {range[0].From}:{range[0].To} и {range[1].From}:{range[1].To}");
+                StringBuilder stringBuilder = new StringBuilder();
+
+                foreach (Range range in ranges)
+                {
+                    stringBuilder.Append(range.ToString()).Append(", ");
+                }
+
+                string line = stringBuilder.Remove(stringBuilder.Length - 2, 2).ToString();
+
+                Console.Write($"[{line}]");
+                Console.WriteLine();
             }
         }
 
         static void Main()
         {
             // Курсовая 1. Часть 1
-            Console.Write("Укажите начало диапазона: ");
+            /*Console.Write("Укажите начало диапазона: ");
             double from = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Укажите конец диапазона: ");
@@ -60,43 +67,72 @@ namespace Academits.Gudkov.RangeTask
             range.To = 100;
 
             Console.WriteLine($"Установлены новые значения диапазона: From = {range.From}, To = {range.To}");
-
+            */
             // Task 2
             // Курсовая 1. Часть 2
-            while (true)
-            {
-                Console.WriteLine("Укажите границы диапазонов");
-                Console.WriteLine();
+            /* while (true)
+             {
+                 Console.WriteLine("Укажите границы диапазонов");
+                 Console.WriteLine();
 
-                Console.Write("Начало первого диапазона: ");
-                double rangeFrom = Convert.ToDouble(Console.ReadLine());
+                 Console.Write("Начало первого диапазона: ");
+                 double rangeFrom = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Конец первого диапазона: ");
-                double rangeTo = Convert.ToDouble(Console.ReadLine());
-                Range range1 = new Range(rangeFrom, rangeTo);
-                Console.WriteLine();
+                 Console.Write("Конец первого диапазона: ");
+                 double rangeTo = Convert.ToDouble(Console.ReadLine());
+                 Range range1 = new Range(rangeFrom, rangeTo);
+                 Console.WriteLine();
 
 
-                Console.Write("Начало второго диапазона: ");
-                rangeFrom = Convert.ToDouble(Console.ReadLine());
+                 Console.Write("Начало второго диапазона: ");
+                 rangeFrom = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Конец второго диапазона: ");
-                rangeTo = Convert.ToDouble(Console.ReadLine());
-                Range range2 = new Range(rangeFrom, rangeTo);
-                Console.WriteLine();
+                 Console.Write("Конец второго диапазона: ");
+                 rangeTo = Convert.ToDouble(Console.ReadLine());
+                 Range range2 = new Range(rangeFrom, rangeTo);
+                 Console.WriteLine();
 
-                Console.WriteLine("Результат пересечения");
-                Print(range1.GetIntersection(range2));
-                Console.WriteLine();
+                 Range range1 = new Range(rangeFrom, rangeTo);
 
-                Console.WriteLine("Результат объединения");
-                Print(range1.GetUnion(range2));
-                Console.WriteLine();
+                 Console.WriteLine("Результат пересечения");
+                 Console.WriteLine(range1.GetIntersection(range2));
 
-                Console.WriteLine("Результат разности");
-                Print(range1.GetDifference(range2));
-                Console.WriteLine("------------------------------------------------------");
-            }
+                 Console.WriteLine("Результат объединения");
+                 Print(range1.GetUnion(range2));
+                 Console.WriteLine();
+
+                 Console.WriteLine("Результат разности");
+                 Print(range1.GetDifference(range2));
+                 Console.WriteLine("------------------------------------------------------");
+            */
+            Range range1 = new Range(1, 10);
+            Range range2 = new Range(2, 8);
+
+            Console.WriteLine("Результат пересечения");
+            //Console.WriteLine(range1.GetIntersection(range2));
+            Print(range1.GetIntersection(range2));
+            Console.WriteLine();
+
+            Console.WriteLine("Результат объединения");
+            Print(range1.GetUnion(range2));
+            Console.WriteLine();
+
+            Console.WriteLine("Результат разности");
+            Print(range1.GetDifference(range2));
+
+            Range range3 = new Range(1, 10);
+            Range range4 = new Range(1, 10);
+
+            Console.WriteLine("Результат разности");
+            Print(range3.GetDifference(range4));
+
+            Range range5 = new Range(1, 10);
+            Range range6 = new Range(2, 10);
+
+            Console.WriteLine("Результат разности");
+            Print(range5.GetDifference(range6));
+
         }
     }
 }
+//}
