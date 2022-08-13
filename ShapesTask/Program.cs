e2a60344504709c14a5add829758b5ab6dbb62e1
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-14 00:57:06 +0700

    Предварительно

--- ShapesTask/Program.cs
+++ ShapesTask/Program.cs
@@ -7,18 +7,26 @@ namespace Academits.Gudkov.ShapesTask
         static void Main()
         {
             // Курсовая 1. Задача 1. Часть 1
+            IShapes triangle1 = new Triangle(new Point[] { new Point(2, 4), new Point(1, 5), new Point(12, 41) });
 
-            IShapes triangle = new Triangle(new Point[] { new Point(2, 4), new Point(1, 5), new Point(12, 41) });
-            Point[] point1 = new Point[3];
-            IShapes triangle2 = new Triangle(point1);
+            Console.WriteLine(triangle1.GetWidth());
+
+            Console.WriteLine(triangle1.GetHeight());
+
+            Console.WriteLine(triangle1.GetPerimeter());
+
+            Console.WriteLine(triangle1.GetArea());
+
+            Point[] point2 = new Point[3];
+            IShapes triangle2 = new Triangle(point2);
 
             Console.WriteLine(triangle2.GetWidth());
 
-            // Console.WriteLine(triangle1.GetHeight());
+            Console.WriteLine(triangle2.GetHeight());
 
-            // Console.WriteLine(triangle1.GetPerimeter());
+            Console.WriteLine(triangle2.GetPerimeter());
 
-            // Console.WriteLine(triangle1.GetArea());
+            Console.WriteLine(triangle2.GetArea());
         }
     }
 }
