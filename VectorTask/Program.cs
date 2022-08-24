6122ae11a797274e110ff943621c9c6a0a466dff
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-08-24 12:18:19 +0700

    VectorTask. Конструкторы

---
+++ VectorTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Academits.Gudkov.VectorTask
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double[] points = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+            Vector vector1 = new Vector(1, points);
+
+            Vector vector2 = new Vector(1, points);
+
+            Console.WriteLine(ReferenceEquals(vector1.Points, vector2.Points));
+
+            Console.WriteLine(vector1);
+            Console.WriteLine(vector2);
+
+        }
+    }
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Academits.Gudkov.VectorTask
+{
+    public class Vector
+    {
+        public double[] Points { get; private set; }
+
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException($"Недопустимые аргументы: размерность вектора = {n}");
+            }
+
+            Points = new double[n];
+        }
+
+        public Vector(Vector vector)
+        {
+            Points = new double[vector.GetSize()];
+            double[] sourseVectorPoints = vector.Points;
+
+            for (int i = 0; i < Points.Length; ++i)
+            {
+                Points[i] = sourseVectorPoints[i];
+            }
+        }
+
+        public Vector(double[] points)
+        {
+            if (points is null)
+            {
+                throw new ArgumentException("Недопустимые аргументы: массив = null");
+            }
+
+            if (points.Length == 0)
+            {
+                throw new ArgumentException($"Недопустимые аргументы: размер массива = 0");
+            }
+
+            Points = new double[points.Length];
+
+            for (int i = 0; i < points.Length; ++i)
+            {
+                Points[i] = points[i];
+            }
+        }
+
+        public Vector(int n, double[] points)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException($"Недопустимые аргументы: размерность вектора = {n}");
+            }
+
+            if (points is null)
+            {
+                throw new ArgumentException("Недопустимые аргументы: массив = null");
+            }
+
+            if (points.Length == 0)
+            {
+                throw new ArgumentException($"Недопустимые аргументы: размер массива = 0");
+            }
+
+            Points = new double[n];
+
+            for (int i = 0; (i < n && i < points.Length); ++i)
+            {
+                Points[i] = points[i];
+            }
+        }
+
+        public int GetSize()
+        {
+            return Points.Length;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder vectorInfo = new StringBuilder("{");
+
+            foreach (double point in Points)
+            {
+                vectorInfo.Append(point).Append(", ");
+            }
+
+            vectorInfo.Remove(vectorInfo.Length - 2, 2).Append('}');
+
+            return vectorInfo.ToString();
+        }
+    }
+}
