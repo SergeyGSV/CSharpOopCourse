0882e0e4552669b73ac8812ae21485e7dee474bf
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-09-05 11:14:28 +0700

    MatrixTask. С определителем

--- MatrixTask/Matrix.cs
+++ MatrixTask/Matrix.cs
@@ -72,7 +72,35 @@ namespace Academits.Gudkov.MatrixTask
 
         public override bool Equals(object obj)
         {
-            return base.Equals(obj);
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Matrix matrix = (Matrix)obj;
+
+            if (Vectors.Length != matrix.Vectors.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Vectors.GetLength(0); ++i)
+            {
+                for (int j = 0; j < Vectors.GetLength(1); ++j)
+                {
+                    if (Vectors[i, j] != matrix.Vectors[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
         public override int GetHashCode()
@@ -82,7 +110,168 @@ namespace Academits.Gudkov.MatrixTask
 
         public override string ToString()
         {
-            return base.ToString();
+            StringBuilder matrixInfo = new StringBuilder("{");
+
+            for (int i = 0; i < Vectors.GetLength(0); ++i)
+            {
+                matrixInfo.Append('{');
+
+                for (int j = 0; j < Vectors.GetLength(1); ++j)
+                {
+                    matrixInfo.Append(Vectors[i, j]).Append(", ");
+                }
+
+                matrixInfo.Remove(matrixInfo.Length - 2, 2).Append("}, ");
+            }
+
+            matrixInfo.Remove(matrixInfo.Length - 2, 2).Append('}');
+
+            return matrixInfo.ToString();
         }
+
+        public int GetSize(int x)
+        {
+            return Vectors.GetLength(x);
+        }
+
+        public Vector GetStringVector(int index)
+        {
+            Vector vector = new Vector(Vectors.GetLength(1));
+
+            for (int i = 0; i < Vectors.GetLength(1); ++i)
+            {
+                vector.SetPoint(i, Vectors[index, i]);
+            }
+
+            return vector;
+        }
+
+        public void SetVector(int index, Vector vector)
+        {
+            for (int i = 0; i < vector.GetSize(); ++i)
+            {
+                Vectors[index, i] = vector.GetPoint(i);
+            }
+        }
+
+        public Vector GetСolumnVector(int index)
+        {
+            Vector vector = new Vector(Vectors.GetLength(0));
+
+            for (int i = 0; i < Vectors.GetLength(0); ++i)
+            {
+                vector.SetPoint(i, Vectors[i, index]);
+            }
+
+            return vector;
+        }
+
+        public void TransposeMatrix()
+        {
+            Matrix matrix = new Matrix(Vectors.GetLength(1), Vectors.GetLength(0));
+
+            for (int i = 0; i < Vectors.GetLength(1); ++i)
+            {
+                for (int j = 0; j < Vectors.GetLength(0); ++j)
+                {
+                    matrix.Vectors[i, j] = Vectors[j, i];
+                }
+            }
+
+            Vectors = matrix.Vectors;
+        }
+
+        public void MultiplyByScalar(double scalar)
+        {
+            for (int i = 0; i < Vectors.GetLength(0); ++i)
+            {
+                for (int j = 0; j < Vectors.GetLength(1); ++j)
+                {
+                    Vectors[i, j] *= scalar;
+                }
+            }
+        }
+
+        public double GetDeterminant()
+        {
+            Matrix matrix = new Matrix(Vectors);
+
+            if (matrix.Vectors.GetLength(0) < 2)
+            {
+                return matrix.Vectors[0, 0];
+            }
+
+            if (matrix.Vectors[0, 0] == 0)
+            {
+                int basisMinorIndex = matrix.GetNonZeroBasisMinorIndex();
+
+                if (basisMinorIndex == -1)
+                {
+                    return 0;
+                }
+
+                matrix.SetBasisMinorInFirstLine(basisMinorIndex);
+            }
+
+            matrix.SetColumnZero();
+           
+            return matrix.Vectors[0, 0] * matrix.GetSubmatrix().GetDeterminant();
+        }
+
+        private int GetNonZeroBasisMinorIndex()
+        {
+            for (int i = 0; i < Vectors.GetLength(0); ++i)
+            {
+                if (Vectors[i, 0] != 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void SetBasisMinorInFirstLine(int i)
+        {
+            for (int j = 0; j < Vectors.GetLength(1); ++j)
+            {
+                Vectors[0, j] += Vectors[i, j];
+            }
+        }
+
+        private void SetColumnZero()
+        {
+            for (int i = 1; i < Vectors.GetLength(0); ++i)
+            {
+                double itemCoefficient = -Vectors[i, 0] / (Vectors[0, 0]);
+
+                for (int j = 0; j < Vectors.GetLength(1); ++j)
+                {
+                    Vectors[i, j] += itemCoefficient * Vectors[0, j];
+                }
+            }
+        }
+
+        private Matrix GetSubmatrix()
+        {
+            Matrix matrix = new Matrix(Vectors.GetLength(0) - 1, Vectors.GetLength(1) - 1);
+
+            for (int i = 0; i < matrix.Vectors.GetLength(0); ++i)
+            {
+                for (int j = 0; j < matrix.Vectors.GetLength(1); ++j)
+                {
+                    matrix.Vectors[i, j] = Vectors[i + 1, j + 1];
+                }
+            }
+
+            return matrix;
+        }
+
+
+
     }
 }
+
+/*
+ 
+ */
--- MatrixTask/Program.cs
+++ MatrixTask/Program.cs
@@ -7,9 +7,9 @@ namespace Academits.Gudkov.MatrixTask
     {
         static void Main(string[] args)
         {
-            Matrix matrix1 = new Matrix(4, 10);
+            /*Matrix matrix1 = new Matrix(4, 10);
 
-            Console.WriteLine(matrix1.Vectors.GetLength(0));
+            /*Console.WriteLine(matrix1.Vectors.GetLength(0));
             Console.WriteLine(matrix1.Vectors.GetLength(1));
 
             for (int i = 0; i < matrix1.Vectors.GetLength(0); ++i)
@@ -19,7 +19,7 @@ namespace Academits.Gudkov.MatrixTask
                     matrix1.Vectors[i, j] = 1;
                 }
             }
-
+            
             Matrix matrix2 = new Matrix(matrix1);
 
             double[,] vectorsArray1 = new double[,]
@@ -29,21 +29,53 @@ namespace Academits.Gudkov.MatrixTask
             };
 
             Matrix matrix3 = new Matrix(vectorsArray1);
-
-            Vector vector1 = new Vector(new double[] { 1, 1, 1 });
-            Vector vector2 = new Vector(new double[] { 1, 1, 1, 1, 1, 1 });
-            Vector vector3 = new Vector(new double[] { 1, 1, 1, 1, });
+            */
+            Vector vector1 = new Vector(new double[] { 0, 3, -1, 2, 6 });
+            Vector vector2 = new Vector(new double[] { 2, 1, 0, 0, 3 });
+            Vector vector3 = new Vector(new double[] { -2, -1, 0, 2, 5 });
+            Vector vector4 = new Vector(new double[] { -5, 7, 1, 1, 1 });
+            Vector vector5 = new Vector(new double[] { 2, 0, 2, -2, 1 });
 
             Vector[] vectorsArray2 = new Vector[]
             {
                 vector1,
                 vector2,
-                vector3
+                vector3,
+                vector4,
+                vector5
             };
 
             Matrix matrix4 = new Matrix(vectorsArray2);
 
+            Console.WriteLine(matrix4.GetDeterminant());
+
+            matrix4.TransposeMatrix();
+
+            Console.WriteLine(matrix4.GetDeterminant());
+
+
+            /*
             Console.WriteLine(vectorsArray2.Length);
+
+            Console.WriteLine(matrix4);
+           
+
+            Console.WriteLine(matrix4.GetStringVector(2));
+           
+
+            matrix4.SetVector(2, vector2);
+
+            Console.WriteLine(matrix4);
+
+            //Console.WriteLine(matrix4.GetСolumnVector(0));
+
+            matrix4.TransposeMatrix();
+
+            Console.WriteLine(matrix4);
+
+            matrix4.MultiplyByScalar(2);
+
+            Console.WriteLine(matrix4);*/
 
         }
     }
