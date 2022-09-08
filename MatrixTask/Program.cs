43377ee49efaeb3028923a40975e58107c32cf18
SergeyGSV <Sergey.V.Gudkov@gmail.com>
2022-09-08 11:53:32 +0700

    MatrixTask. Завершено, на проверку

--- MatrixTask/Program.cs
+++ MatrixTask/Program.cs
@@ -18,7 +18,8 @@ namespace Academits.Gudkov.MatrixTask
 
                 return;
             }
-            else if (matrixArray.Length == 0)
+
+            if (matrixArray.Length == 0)
             {
                 Console.WriteLine("Печать невозможна: размер массива (matrixArray) = 0");
 
@@ -29,15 +30,19 @@ namespace Academits.Gudkov.MatrixTask
             {
                 for (int j = i + 1; j < matrixArray.GetLength(0) - 1; ++j)
                 {
-                    Console.WriteLine($"Матрицы: {i + 1}-{j + 1} | Equals / HashCode : {matrixArray[i].Equals(matrixArray[j])} / {IsEqualHashCode(matrixArray[i], matrixArray[j])}");
+                    bool isEquals = matrixArray[i].Equals(matrixArray[j]);
+                    bool isEqualHashCode = IsEqualHashCode(matrixArray[i], matrixArray[j]);
+
+                    Console.WriteLine("Матрицы: {0,2} - {1,2} | Equals / HashCode : {2,5} / {3,5}", i + 1, j + 1, isEquals, isEqualHashCode);
                 }
             }
         }
+
         public static void PrintHashCodes(Matrix[] matrixArray)
         {
             for (int i = 0; i < matrixArray.GetLength(0); ++i)
             {
-                Console.WriteLine($"Матрица{i + 1}: {matrixArray[i].GetHashCode()}");
+                Console.WriteLine("Матрица {0,2} : {1,11}", i + 1, matrixArray[i].GetHashCode());
             }
         }
 
@@ -46,16 +51,22 @@ namespace Academits.Gudkov.MatrixTask
             Matrix matrix1 = new Matrix(5, 5);
             Matrix matrix2 = new Matrix(matrix1);
 
+            Console.WriteLine(matrix2);
+            Console.WriteLine($"Размер матрицы 2: {matrix2.GetStringCount()} x {matrix2.GetColumnCount()} {Environment.NewLine}");
+
             double[,] vectorsArray1 =
-            {
-                { 0, 3, -1, 2, 6 },
-                { 2, 1, 0, 0, 3 },
-                { -2, -1, 0, 2, 5 },
-                { -5, 7, 1, 1, 1 },
-                { 2, 0, 2, -2, 1 }
+           {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
             };
 
             Matrix matrix3 = new Matrix(vectorsArray1);
+
+            Console.WriteLine($"{matrix3} - исходная матрица");
+
+            Console.WriteLine($"{matrix3.GetStringVector(1)} - вектор-строки матрицы 3");
+            Console.WriteLine($"{matrix3.GetСolumnVector(1)} - вектор-столбца матрицы 3 {Environment.NewLine}");
 
             Vector[] vectorsArray2 =
             {
@@ -67,102 +78,179 @@ namespace Academits.Gudkov.MatrixTask
             };
 
             Matrix matrix4 = new Matrix(vectorsArray2);
+            Vector vector1 = new Vector(new double[] { 0, 0, 0, 0, 0 });
 
-            Console.WriteLine(matrix1);
-            Console.WriteLine(matrix2);
-            Console.WriteLine(matrix3);
-            Console.WriteLine(matrix4);
+            Console.WriteLine($"{matrix4} - исходная матрица");
 
-            Matrix[] matrixArray =
-            {
-                matrix1,
-                matrix2,
-                matrix3,
-                matrix4
-            };
+            matrix4.SetVector(2, vector1);
 
-            PrintComparisonResult(matrixArray);
-            PrintHashCodes(matrixArray);
-
-            Console.WriteLine($"Размер матрицы 1: {matrix1.GetStringCount()} x {matrix1.GetColumnCount()}");
-
-            Console.WriteLine($"{matrix3.GetStringVector(2)} - вектор-строки матрицы 3");
-
-            Console.WriteLine($"{matrix3.GetСolumnVector(2)} - вектор-столбца матрицы 3");
-
-            Matrix matrix5 = new Matrix(matrix4);
-            Vector vector = new Vector(matrix3.GetСolumnVector(2));
-
-            Console.WriteLine(matrix5);
-            matrix5.SetVector(2, vector);
-            Console.WriteLine($"{matrix5} - задан вектор-строка {vector} по индексу 2");
+            Console.WriteLine($"{matrix4} - задан вектор-строка {vector1} по индексу 2 {Environment.NewLine}");
 
             double[,] vectorsArray3 =
-            {
+{
                 { 1, 2, 3 },
                 { 4, 5, 6 },
                 { 7, 8, 9 }
             };
 
-            Matrix matrix6 = new Matrix(vectorsArray3);
-            Console.WriteLine($"{matrix6} - исходная матрица");
-            matrix6.TransposeMatrix();
-            Console.WriteLine($"{matrix6} - транспонированная матрица");
+            Matrix matrix5 = new Matrix(vectorsArray3);
 
-            matrix4.MultiplyByScalar(2);
-            Console.WriteLine($"{matrix4} - умножение на скаляр (2)");
+            Console.WriteLine($"{matrix5} - исходная матрица");
 
-            Console.WriteLine($"{matrix3.GetDeterminant():f2} - определитель матрицы");
+            matrix5.TransposeMatrix();
+
+            Console.WriteLine($"{matrix5} - транспонированная матрица");
+
+            matrix5.MultiplyByScalar(2);
+
+            Console.WriteLine($"{matrix5} - результат умножения на скаляр (2) {Environment.NewLine}");
 
             double[,] vectorsArray4 =
             {
+                { 0, 3, -1, 2, 6 },
+                { 2, 1, 0, 0, 3 },
+                { -2, -1, 0, 2, 5 },
+                { -5, 7, 1, 1, 1 },
+                { 2, 0, 2, -2, 1 }
+            };
+
+            double[,] vectorsArray5 =
+            {
+                { 0, 1, -2 },
+                { -1, 2, 3 },
+                { 2, 3, 4 },
+            };
+
+            Matrix matrix6 = new Matrix(vectorsArray4);
+
+            Console.WriteLine($"{matrix6} - исходная матрица");
+
+            Console.WriteLine($"{matrix6.GetDeterminant():f2} - определитель матрицы {Environment.NewLine}");
+
+            double[,] vectorsArray6 =
+{
                 { 1 },
                 { 1 },
                 { 1 }
             };
 
-            matrix4 = new Matrix(vectorsArray4);
-            matrix4.MultiplyByStringVector(new Vector(new double[] { 2, 2, 2 }));
-            Console.WriteLine($"{matrix4} - умножение на вектор-строку");
+            Matrix matrix7 = new Matrix(vectorsArray6);
+            Vector vector2 = new Vector(new double[] { 2, 2, 2 });
 
-            double[,] vectorsArray5 =
+            Console.WriteLine($"{matrix7} - исходная матрица");
+
+            matrix7.MultiplyByStringVector(new Vector(vector2));
+
+            Console.WriteLine($"{matrix7} - умножение на вектор-строку {vector2} {Environment.NewLine}");
+
+            double[,] vectorsArray7 =
             {
                 { 1, 1, 1 },
                 { 1, 1, 1 },
                 { 1, 1, 1 }
             };
 
-            matrix4 = new Matrix(vectorsArray5);
-            matrix4.MultiplyByColumnVector(new Vector(new double[] { 2, 2, 2 }));
-            Console.WriteLine($"{matrix4} - умножение на вектор-столбец");
+            Matrix matrix8 = new Matrix(vectorsArray7);
+            Vector vector3 = new Vector(new double[] { 2, 2, 2 });
 
-            double[,] vectorsArray6 =
+            Console.WriteLine($"{matrix8} - исходная матрица");
+
+            matrix8.MultiplyByColumnVector(vector3);
+
+            Console.WriteLine($"{matrix8} - умножение на вектор-столбец {vector3} {Environment.NewLine}");
+
+            double[,] vectorsArray8 =
             {
                 { 4, 4, 1 },
                 { 4, 4, 1 },
                 { 4, 4, 1 }
             };
 
-            Matrix matrix7 = new Matrix(vectorsArray6);
-            Matrix matrix8 = new Matrix(matrix7);
-            matrix8.Add(matrix7);
-            Console.WriteLine($"{matrix8} - сложение матриц");
+            double[,] vectorsArray9 =
+            {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+            };
 
-            matrix8.Subtract(matrix7);
-            Console.WriteLine($"{matrix8} - вычитание матриц");
+            Matrix matrix9 = new Matrix(vectorsArray8);
+            Matrix matrix10 = new Matrix(vectorsArray9);
 
-            Matrix matrix9 = new Matrix(1, 1);
+            Console.WriteLine($"{matrix9} - исходная матрица");
+            Console.WriteLine($"{matrix10} - исходная матрица");
 
-            matrix9 = Matrix.GetSum(matrix7, matrix8);
-            Console.WriteLine($"{matrix9} - сложение матриц, статический метод");
+            matrix9.Add(matrix10);
 
-            Matrix matrix10 = new Matrix(1, 1);
+            Console.WriteLine($"{matrix9} - результат сложения {Environment.NewLine}");
 
-            matrix10 = Matrix.GetDifference(matrix9, matrix8);
-            Console.WriteLine($"{matrix9} - вычитание матриц, статический метод");
+            Console.WriteLine($"{matrix9} - исходная матрица");
+            Console.WriteLine($"{matrix10} - исходная матрица");
 
-            matrix10 = Matrix.GetMultiplyResult(matrix9, matrix8);
-            Console.WriteLine($"{matrix10} - умножение матриц, статический метод");
+            matrix9.Subtract(matrix10);
+
+            Console.WriteLine($"{matrix9} - результат вычитания {Environment.NewLine}");
+
+            double[,] vectorsArray10 =
+           {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+            };
+
+            double[,] vectorsArray11 =
+{
+                { 4, 4, 4 },
+                { 4, 4, 4 },
+                { 4, 4, 4 }
+            };
+
+            Matrix matrix11 = new Matrix(vectorsArray10);
+            Matrix matrix12 = new Matrix(vectorsArray11);
+
+            Matrix matrix13 = new Matrix(1, 2);
+
+            Console.WriteLine($"{matrix11} - исходная матрица");
+            Console.WriteLine($"{matrix12} - исходная матрица");
+
+            matrix13 = Matrix.GetSum(matrix11, matrix12);
+
+            Console.WriteLine($"{matrix13} - результат сложения, статический метод {Environment.NewLine}");
+
+            Console.WriteLine($"{matrix13} - исходная матрица");
+            Console.WriteLine($"{matrix11} - исходная матрица");
+
+            matrix13 = Matrix.GetDifference(matrix13, matrix11);
+
+            Console.WriteLine($"{matrix13} - результат вычитания, статический метод {Environment.NewLine}");
+
+            Console.WriteLine($"{matrix11} - исходная матрица");
+            Console.WriteLine($"{matrix12} - исходная матрица");
+
+            matrix13 = Matrix.GetMultiplyResult(matrix11, matrix12);
+
+            Console.WriteLine($"{matrix13} - результат умножения, статический метод {Environment.NewLine}");
+
+            Matrix[] matrixArray =
+            {
+                matrix1,
+                matrix2,
+                matrix3,
+                matrix4,
+                matrix5,
+                matrix6,
+                matrix7,
+                matrix8,
+                matrix9,
+                matrix10,
+                matrix11,
+                matrix12,
+                matrix13
+            };
+
+            PrintComparisonResult(matrixArray);
+            Console.WriteLine();
+
+            PrintHashCodes(matrixArray);
         }
     }
 }
